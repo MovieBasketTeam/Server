6a6ff79f6fb392bc1e70fe475babcc973c5d6ad0
hsubeen <ghd-tnqls4962@hanmail.net>
2016-12-29 03:05:23 +0900

    logout
    logout get method in member.js

--- movie_basket/routes/member.js
+++ movie_basket/routes/member.js
@@ -8,26 +8,27 @@ router.get('/', function (req, res) {
     res.send({result : 'check'});
 });
 
+router.get('/logout', function(req,res,next){
+    req.session.destroy(function(err){});
+    var results = {message : 'logout success'};
+    res.send({result : results});
+});
+
 // 로그인 '/' post 방식 요청 처리
 router.post('/', function (req, res, next) {
-    var logInInfo = {
+    var logOutInfo = {
         member_email : req.body.member_email,
         member_pwd : req.body.member_pwd
     };
 
-    Member.logIn(logInInfo, function (error, results) {
-        if (error) {
-            console.log("Connection error " + error);
-            return res.send(error);
-        }
-        else {
-            if (results.member_info) {
-                var sess = req.session;
-                sess.member_id = results.member_info.member_id;
-                sess.member_name = results.member_info.member_name;
-            }
-        }
-        res.status(201).send({result : results.message});
+    Member.logIn(logOutInfo, function (error, results) {
+      if (error) {
+          console.log("Connection error " + error);
+          res.send(error);
+      }
+      else {
+          res.status(200).send({result : results});
+      }
     });
 });
 
