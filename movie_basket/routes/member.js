9886302ac93dfd42088988ce2d0bda91922bd0c3
george1218 <n690102@naver.com>
2016-12-28 16:33:48 +0900

    session update
    redis fail but local is okay

--- movie_basket/routes/member.js
+++ movie_basket/routes/member.js
@@ -2,6 +2,12 @@ var express = require('express');
 var Member = require('../models/member');
 var router = express.Router();
 
+
+router.get('/', function (req, res) {
+    console.log(req.session.member_name);
+    res.send({result : 'check'});
+});
+
 // 로그인 '/' post 방식 요청 처리
 router.post('/', function (req, res, next) {
     var logInInfo = {
@@ -12,11 +18,20 @@ router.post('/', function (req, res, next) {
     Member.logIn(logInInfo, function (error, results) {
         if (error) {
             console.log("Connection error " + error);
-            res.send(error);
+            return res.send(error);
         }
         else {
-            res.status(201).send({result : results});
+            if (results.member_info) {
+                var sess = req.session;
+                sess.member_id = results.member_info.member_id;
+                sess.member_name = results.member_info.member_name;
+                console.log("session made " +req.session);
+            }
+    //        res.status(201).send({result : results.message});
         }
+        console.log("results message : "+results.message);
+        res.status(201).send({result : results.message});
+        console.log("results message : "+results.message);
     });
 });
 
