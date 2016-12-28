a465721579c7dbf95ddd7dfebf730dc83e0e0734
hsubeen <ghd-tnqls4962@hanmail.net>
2016-12-28 23:53:56 +0900

    subeen
    subeen

--- movie_basket/models/search.js
+++ movie_basket/models/search.js
@@ -5,10 +5,27 @@ var async = require('async');
 
 //category 검색 메인 화면
 function category () {
-
+  var sql_category_todayRecommend = 'SELECT * FROM category AS c WHERE c.today_recommand = 1';
+  dbPool.getConnection(function(error,dbConn){
+    if(error){
+      return callback(error);
+    }
+    var result = {};
+    dbConn.query(sql_category_todayRecommend, function(err,rows){
+      if(error){
+        dbConn.release();
+        return done(error);
+      }
+      else if(rows.length > 0){
+        dbConn.release();
+        result = { today_recommand : rows};
+        return callback(null, result);
+      }
+    });
+  });
 }
 
-//ㅋ테고리 선택 후 밧켓 조회 함수
+//카테고리 선택 후 밧켓 조회 함수
 function detailCategory (c_id) {
   var sql_detail_category = '';
   dbPool.getConnection(function(error,dbConn){
