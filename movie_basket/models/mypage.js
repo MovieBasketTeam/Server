5945138bb10ea494f66eb6dd96930da41a4a424e
jaeleum <kjl0019@gmail.com>
2017-01-01 00:24:58 +0900

    modify query
    update movieCart in mypage.js

--- movie_basket/models/mypage.js
+++ movie_basket/models/mypage.js
@@ -77,25 +77,11 @@ function movieCart(mypageInfo, callback) {
   var sql_movieCart =
   'SELECT m.movie_id, m.movie_title, m.movie_image, m.movie_director, m.movie_pub_date, m.movie_adder, '+
   'm.movie_user_rating, m.movie_link, m.movie_like, (CASE WHEN mh.u_id IS NULL THEN 0 ELSE 1 END) AS is_liked, '+
-  '(CASE WHEN mc.u_id IS NULL THEN 0 ELSE 1 END) AS is_cart '+
+  '(CASE WHEN mc.u_id IS NULL THEN 0 ELSE 1 END) AS is_cart , bn.basket_name'+
   'FROM movie m JOIN (SELECT m_id, u_id FROM movie_clip WHERE u_id = ?) mc ON(m.movie_id = mc.m_id) '+
   'LEFT JOIN (SELECT m_id, u_id FROM movie_heart WHERE u_id = ?) mh ON(m.movie_id = mh.m_id) '+
   'JOIN (SELECT basket_name, basket_id FROM basket) AS bn ON(m.basket_id = bn.basket_id)';
 
-
-
-
-
-
-
-
-
-  'SELECT movie_id, movie_title, movie_image, movie_director, movie_pub_date, movie_adder, ' +
-  'movie_user_rating, movie_link, movie_like, (CASE WHEN u_id IS NULL THEN 0 ELSE 1 END) AS is_liked, ' +
-  '(CASE WHEN m.movie_id=mc.m_id THEN 1 ELSE 0 END) AS is_cart '+
-  'FROM movie m JOIN movie_clip mc ON (m.movie_id = mc.m_id) ' +
-               'JOIN member mem ON (mc.u_id = mem.member_id) ' +
-  'WHERE mem.member_id = ?';
   dbPool.getConnection(function(error, dbConn) {
     if(error) {
       return callback(error);
@@ -106,7 +92,8 @@ function movieCart(mypageInfo, callback) {
             dbConn.release();
             return callback(error);
         }
-    dbConn.query(sql_movieCart, [jwt.decodeToken(mypageInfo.member_token).member_id], function(error, rows) {
+        var member_id = jwt.decodeToken(mypageInfo.member_token).member_id;
+    dbConn.query(sql_movieCart, [member_id, member_id], function(error, rows) {
       if (error) {
         return dbConn.rollback(function () {
             dbConn.release();
@@ -125,19 +112,9 @@ function movieCart(mypageInfo, callback) {
 
 function movieRecommend(mypageInfo, callback) {
   var sql_movieReccomend =
-  /*
-  'SELECT m.movie_id, m.movie_title, m.movie_image, m.movie_director, m.movie_pub_date,  m.movie_adder, ' +
-  'm.movie_user_rating, m.movie_link, m.movie_like, (CASE WHEN mh.u_id IS NULL THEN 0 ELSE 1 END) AS is_liked, ' +
-  '(CASE WHEN m.movie_id=mc.m_id THEN 1 ELSE 0 END) AS is_cart '+
-  'FROM movie m JOIN movie_heart mh ON (m.movie_id = mh.m_id) ' +
-  'JOIN member mem ON (mh.u_id = mem.member_id) ' +
-  'JOIN movie_clip mc ON(mc.u_id = mem.member_id) ' +
-  'WHERE mem.member_id = ?';
-  */
-
   'SELECT m.movie_id, m.movie_title, m.movie_image, m.movie_director, m.movie_pub_date, m.movie_adder, '+
   'm.movie_user_rating, m.movie_link, m.movie_like, (CASE WHEN mh.u_id IS NULL THEN 0 ELSE 1 END) AS is_liked, '+
-  '(CASE WHEN mc.u_id IS NULL THEN 0 ELSE 1 END) AS is_cart '+
+  '(CASE WHEN mc.u_id IS NULL THEN 0 ELSE 1 END) AS is_cart, bn.basket_name'+
   'FROM movie m JOIN (SELECT m_id, u_id FROM movie_heart WHERE u_id = ?) mh ON(m.movie_id = mh.m_id) '+
   'LEFT JOIN (SELECT m_id, u_id FROM movie_clip WHERE u_id = ?) mc ON(m.movie_id = mc.m_id) '+
   'JOIN (SELECT basket_name, basket_id FROM basket) AS bn ON(m.basket_id = bn.basket_id)';
