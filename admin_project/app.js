2b2a8bcb8beb25ba1349cc924f941e12cf3d03eb
Kwon Minha <minha61@naver.com>
2017-01-01 16:02:31 +0900

    admin_project

--- admin_project/app.js
+++ admin_project/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var basketRank = require('./routes/basketRank');
+var category = require('./routes/category');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/images', express.static(path.join(__dirname, 'uploads/images')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/basketRank', basketRank);
+app.use('/category', category);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
