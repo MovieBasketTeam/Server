7d0fd9f0d6ab8b048c46c4da601808fc50490560
hsubeen <ghd-tnqls4962@hanmail.net>
2017-01-05 01:30:43 +0900

    ss
    ss

--- admin_project/routes/login.js
+++ admin_project/routes/login.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var awsinfo_config = require('../config/awsinfo_config.json');
+
+var url = awsinfo_config.url;
 
 router.get('/', function (req, res, next) {
-    res.render('login');
+    res.render('login',
+    {
+        urls : url
+    });
 });
 
 module.exports = router;
