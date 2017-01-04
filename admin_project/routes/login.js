ea7298709104133ea2f6e25b7fede7645add2a85
hsubeen <ghd-tnqls4962@hanmail.net>
2017-01-05 05:35:46 +0900

    a
    aaa

--- admin_project/routes/login.js
+++ admin_project/routes/login.js
@@ -4,10 +4,36 @@ var router = express.Router();
 var db_config = require('../config/db_config.json');
 var awsinfo_config = require('../config/awsinfo_config.json');
 var url = awsinfo_config.url;
+var Member = require('../models/login');
 router.get('/', function (req, res, next) {
-
     res.render('login', { urls : url });
 });
 
+// 로그인 '/' post 방식 요청 처리
+router.post('/', function (req, res, next) {
+    var logInInfo = {
+        member_email : req.body.id,
+        member_pwd : req.body.pwd
+    };
+    Member.logIn(logInInfo, function (error, results) {
+        if (error) {
+            console.log("Connection error " + error);
+            return res.send(error);
+        }
+        else if (results.message == 'check information') {
+            res.status(201).render('login', {urls :url});
+        }
+        else {
+            if (results.member_info) {
+                var sess = req.session;
+                console.log(req.session);
+                sess.member_id = results.member_info.member_email;
+                var result_value = {message : results.message};
+                res.status(201).render('main', {urls : url});
+            }
+        }
 
+
+    });
+});
 module.exports = router;
